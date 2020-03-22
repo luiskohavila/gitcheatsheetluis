e4b9c267453cbab349981d3fa6da25aab2f253cd
Luis_Koh <fernandokoh1@hotmail.com>
2020-03-21 20:42:05 -0600

    1.9

--- activitieszedshaw/exercise10.c
+++ activitieszedshaw/exercise10.c
@@ -25,3 +25,46 @@ char *states[] = { //Creating an array with those words
     return 0; //To see if everythin goes well
 
 }
+
+/*
+EXTRA CREDITS
+
+1.- Figure out what kind of code you can put into the parts of a for-loop .
+
+It is divided into three parts: the first one is the initialization of the expression,
+then separated by a semicolon is the condition, finally separated by a semicolon is the
+counter, that will add or subtract numbers until the condition.
+
+2.- Look up how to use the ',' (comma) character to separate multiple statements
+in the parts of the for-loop , but between the ';' (semicolon) characters.
+
+The comma is used to separate parameters in a list, and function as loops. On the
+other hand the semicolon is used end all the lines of code in c, and, in loops for
+the separation of the parts.
+
+3.- Read what a NULL is and try to use it in one of the elements of the states
+array to see what it'll print.
+
+It prints (null) in the slot of the array:
+state 0: California
+state 1: (null)
+state 2: Oregon
+state 3: Washington
+
+4.- See if you can assign an element from the states array to the argv array before
+printing both. Try the inverse.
+
+Yes I can, in my case I assigned the argument 1 to the the slot of Texas in the array and when I printed appeared:
+arg 1: Texas
+state 0: California
+state 1: Oregon
+state 2: Washington
+state 3: Texas
+When I tried the inverse I notice that the name of the state change for the argument I wrote:
+arg 1: holaxd
+state 0: California
+state 1: Oregon
+state 2: Washington
+state 3: holaxd
+
+*/
