73387aaa7c99579d15f67f3e53d29cb670a6b225
Luis_Koh <fernandokoh1@hotmail.com>
2020-03-30 01:54:11 -0600

    hangman

---
+++ hangman.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+
+char *list = "coronavirus";
+
+int main(void) {
+   int wordi, length, lives = 5;
+   char word[25];
+
+   int j = 0, correct, i = 0;
+   char symb;
+   srand((unsigned int) time(NULL));
+   printf("\nLet's start. You have %d lives \n", lives);
+   wordi = 0;
+
+   length = strlen(&list[wordi]);
+   for (j = 0; j < length; j++) {
+      word[j] = '*';
+   }
+
+   word[length] = 0;
+   printf("%s\n", word);
+   while ((lives > 0) && (i < length)) {
+      correct = 0;
+      printf("Enter a letter: ");
+      do {
+         scanf("%c", & symb);
+         symb = tolower(symb);
+         if ((symb > ' ') && ((symb < 'a') || (symb > 'z'))) {
+            printf("Invalid character\nEnter a letter: ");
+         }
+      } while ((symb < 'a') || (symb > 'z'));
+      for (j = 0; j < length; j++) {
+         if (symb == word[j]) {
+            printf("This character already used.\n");
+            lives--;
+            printf("Same letter typed. %i lives left.\n", lives);
+            break;
+         }
+      }
+      if (j == length) {
+
+         for (j = 0; j < length; j++) {
+            if (symb == list[j]) {
+               word[j] = symb;
+               i++;
+               correct = 1;
+            }
+         }
+         if (correct == 0) {
+            lives--;
+            if (lives > 0) {
+               printf("Wrong. %i lives left.\n", lives);
+            } else {
+               printf("You lose.\n");
+               return 0;
+            }
+         } else {
+            if (i < length) {
+               printf("Correct.\n");
+            } else {
+               printf("Congratulations!\n");
+            }
+         }
+         printf("You have %s\n", word);
+      }
+   }
+}
